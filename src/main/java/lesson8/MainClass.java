ad58cf3391ed0b338e77939d806dfc1bf16f80a9
AlukovRinat <1000magnet2000@gmail.com>
2023-01-17 21:58:03 +0300

    povtor 8dz

--- src/main/java/lesson8/MainClass.java
+++ src/main/java/lesson8/MainClass.java
@@ -8,36 +8,19 @@ public class MainClass {
 
     public static void main(String[] args) {
         connect();
-        clearTableUsers();
-        createTableUsers();
-        fillTableUsers();
-        showUsers();
-        showUsersByAge(25,32);
-        deleteUserByName("Piter2");
-        showUsers();
+        clearTablePogoda();
+        createTablePogoda();
+        fillTablePogoda();
+        showPogoda();
         disconnect();
     }
-
-    public static void showUsersByAge(int min, int max) {
-        System.out.println("showUsersByAge");
+    public static void showPogoda() {
+        System.out.println("showPogoda");
         try {
-            ResultSet rs = stmt.executeQuery("SELECT * FROM `users` WHERE age >= "+min+" AND age <= "+max+";");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM pogoda;");
             while (rs.next()) {
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+
-                        rs.getInt(3)+"   "+rs.getString(4));
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    public static void showUsers() {
-        System.out.println("showUsers");
-        try {
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users;");
-            while (rs.next()) {
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+
-                        rs.getInt(3)+"   "+rs.getString(4));
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+
+                        rs.getInt(3)+" "+rs.getString(4)+" "+rs.getString(5));
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -45,54 +28,40 @@ public class MainClass {
 
     }
 
-    public static void deleteUserByName(String name){
+       public static void clearTablePogoda(){
         try {
-            stmt.executeUpdate("DELETE FROM `users` WHERE name = '"+name+"';");
+            stmt.executeUpdate("DELETE FROM `pogoda`;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
-    public static void clearTableUsers(){
+    public static void createTablePogoda(){
         try {
-            stmt.executeUpdate("DELETE FROM `users`;");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `pogoda` (`id` INT(5) NOT NULL AUTO_INCREMENT, `city` VARCHAR(15), `localDate` VARCHAR(4), `weatherText` VARCHAR(40),float(5) 'temperature', PRIMARY KEY (`id`));");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
-    public static void createTableUsers(){
-        try {
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `users` (`id` INT(5) NOT NULL AUTO_INCREMENT, `name` VARCHAR(15), `age` INT(4), `email` VARCHAR(40), PRIMARY KEY (`id`));");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    public static void fillTableUsers(){
-        String[] userslist = {"Ivan1 25 ivan125@mail.ru",
-                "Ivan2 35 ivan235@mail.ru",
-                "Bob1 20 bob120@mail.ru",
-                "Piter1 32 piter132@mail.ru",
-                "Piter2 18 piter218@mail.ru",
-                "Nata1 19 nata119@mail.ru",
-        };
-        String[] items;
+    public static void fillTablePogoda(){
+        String[] pogodalist = {"Vladimir 22.02.2022 weatherText, 22"};
+                String[] items;
         try {
             connection.setAutoCommit(false);
-            for (String str: userslist) {
+            for (String str: pogodalist) {
                 items = str.split(" ");
-                stmt.executeUpdate("INSERT INTO `users` (`name`, `age`, `email`) VALUES ('"+
-                        items[0] + "',"+items[1]+",'"+items[2]+"');");
+                stmt.executeUpdate("INSERT INTO `pogoda` (`city`, `localDate`, `weatherText`, 'temperature') VALUES ('"+
+                        items[0] + "',"+items[1]+",'"+items[2]+""+items[3]+"');");
 
             }
             connection.setAutoCommit(true);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-    }
+}
 
-    public static void connect() {
+        public static void connect() {
         // opening database connection to MySQL server
         try {
             // Строка ?serverTimezone=Europe/Moscow нужна для устранения ошибки TimeZone
